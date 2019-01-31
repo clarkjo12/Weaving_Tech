df516bfc792d59a8dac2b59ec85bd01a0241783b
Jon <clarkjo12@gmail.com>
2019-01-30 23:39:39 -0500

    jsons are probably not cool

---
+++ create-react-express/client/src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./LoginForm.css";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <form className="sign-in">
+        <div>
+          <h2>Sign-in/Login</h2>
+        </div>
+        <br />
+        <input className="form-control" placeholder="Username" id="username" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Password"
+          id="password"
+        />
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
