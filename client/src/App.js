add43790f72a929ba9459468469e5e77724b2399
Brendan Cannell <brendan@cannells.com>
2019-01-30 19:43:10 -0500

    Set up react-native-web, remove .env and .yarn-integrity

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+import { Text, View, Image } from 'react-native';
 import logo from "./logo.svg";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View style={{alignItems: "center", flex: 1}}>
+        <View style={{backgroundColor: "#222", height: 190, padding: 20, alignSelf: "stretch", alignItems: "center"}}>
+          <Image source={logo} alt="logo" style={{width: 120, height: 80}}/>
+          <Text style={{fontSize: 36, fontWeight: "bold", margin: 30, color: "white", fontFamily: "sans-serif"}}>
+            Welcome to React Native
+          </Text>
+        </View>
+        <Text style={{fontSize: "large", fontFamily: "sans-serif", margin: 30}}>
+          To get started, edit <Text>src/App.js</Text> and save to reload.
+        </Text>
+      </View>
     );
   }
 }
