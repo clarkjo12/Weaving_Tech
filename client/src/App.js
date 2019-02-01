6c79d5b2a690b30352e2d736557ad07372608262
Jon <clarkjo12@gmail.com>
2019-02-01 18:04:41 -0500

    possibly

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginForm from "./Components/LoginForm/LoginForm";
+import Truckhome from "./pages/Truckhome";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div className="App">
         <div className="App-header">
           <h2>WMFA!?</h2>
@@ -12,9 +15,20 @@ class App extends Component {
         <div className="login-div">
           <LoginForm />
         </div>
+        <div>
+          <Link to="/userhome">
+            <button id="user-login-submit"> User-login </button>
+            <Route exact path="/truckhome" component={Truckhome} />
+          </Link>
+        </div>
+        <div>
+          <Link to="/truckhome">
+            <button id="truck-login-submit"> Truck-login </button>
+          </Link>
+        </div>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
