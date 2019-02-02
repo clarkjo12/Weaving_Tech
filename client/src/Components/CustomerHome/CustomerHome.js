f52eef1ea7369d7e7caefe4485f43fe0d6eeba02
timmy burrito <timothylalbrecht@gmail.com>
2019-02-01 22:55:17 -0500

    added page folders and files

---
+++ client/src/Components/CustomerHome/CustomerHome.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./CustomerHome.css";
+
+class CustomerHome extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Customer Home Page</h2>
+        </div>
+
+    );
+  }
+}
+
+export default CustomerHome;
---
+++ client/src/Components/TruckHome/TruckHome.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./TruckHome.css";
+
+class TruckHome extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Truck Home Landing</h2>
+        </div>
+
+    );
+  }
+}
+
+export default TruckHome;
