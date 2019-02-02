acb64ec16b9dbfcee107b989606811a44c24a538
Hilary Mallar <hilarymallar@gmail.com>
2019-02-02 17:46:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,12 +15,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wmfa");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
