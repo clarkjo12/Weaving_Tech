164c093e475105939acd81deed4a60c9d5cb90f9
eawehner <elizabethawehner@gmail.com>
2019-02-02 14:30:01 -0500

    added example code to experiment with map feature

--- client/src/Components/CustomerHome/CustomerHome.js
+++ client/src/Components/CustomerHome/CustomerHome.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
 import "./CustomerHome.css";
+import CustomerMap from "../CustomerMap/CustomerMap";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class CustomerHome extends Component {
+  static defaultProps = {
+    center: {
+      lat: 35.91,
+      lng: -79.05
+    },
+    zoom: 11
+  };
+
   render() {
     return (
-        <div>
-          <h2>Customer Home Page</h2>
-        </div>
-
+      <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact 
+        bootstrapURLKeys={{ key: /* API KEY HERE */}}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={35.913200}
+          lng={-79.055847}
+          text={'Chapel Hill'}
+        />
+      </GoogleMapReact>
+    </div>
     );
   }
 }
--- client/src/Components/CustomerMap/CustomerMap.js
+++ client/src/Components/CustomerMap/CustomerMap.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import "./CustomerMap.css";
 
 class CustomerMap extends Component {
+
   render() {
     return (
         <div>
-          <h2>Customer Map Page</h2>
-        </div>
 
+        </div>
     );
   }
 }
