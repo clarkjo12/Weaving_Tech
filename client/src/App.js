27d87453a532d681b53d018b7598e19b6ff7de30
timmy burrito <timothylalbrecht@gmail.com>
2019-02-01 22:56:04 -0500

    added router-dom and route pathways

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import LoginForm from "./Components/LoginForm/LoginForm";
+import CustomerHome from "./Components/CustomerHome/CustomerHome";
+import CustomerMap from "./Components/CustomerMap/CustomerMap";
+import TruckHome from "./Components/TruckHome/TruckHome";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>WMFA!?</h2>
-        </div>
-        <div className="login-div">
-          <LoginForm />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={LoginForm} exact />
+          <Route path="/truck" component={TruckHome} />
+          <Route path="/customer" component={CustomerHome} />
+          <Route path="/map" component={CustomerMap} />
+          <Route component={ErrorPage} />
+        </Switch> 
+      </BrowserRouter>
     );
   }
 }
