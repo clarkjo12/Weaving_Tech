70477d15a8300432e1c148b7d690aa6821a7e13e
Jon <clarkjo12@gmail.com>
2019-02-07 05:11:15 -0500

    favorited all buttons added to map

--- client/src/components/MapButtons.js
+++ client/src/components/MapButtons.js
@@ -19,15 +19,40 @@ import styled from "styled-components";
 //   background: #ffbd59;
 // `;
 
-const Component = styled.div`
-  color: red;
+const Main = styled.div`
+  display: flex;
+  padding-left: 42%;
+  margin-top: -2em;
+`;
+
+const FavButton = styled.button`
+  background: tomato;
+  color: white;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: solid 0;
+  z-index: 3;
+  position: relative;
+`;
+
+const AllButton = styled.button`
+  background: lightseagreen;
+  color: white;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: solid 0;
+  z-index: 3;
+  position: relative;
 `;
 
 function MapButtons(props) {
   return (
-    <Component as="button" onClick={() => alert("It works!")}>
-      Hello World!
-    </Component>
+    <Main>
+      <FavButton>Favorited</FavButton>
+      <AllButton>All</AllButton>
+    </Main>
   );
 }
 
