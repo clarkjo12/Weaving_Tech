146f5877ce630e2911077b3e86118a97119ae463
timmy burrito <timothylalbrecht@gmail.com>
2019-02-08 21:00:41 -0500

    added burger nav, styling, and routing

---
+++ client/src/components/sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { bubble as Menu } from "react-burger-menu";
+
+export default props => {
+  return (
+    <Menu right >
+      <a className="menu-item" href="/">
+        Landing
+      </a>
+
+      <a className="menu-item" href="/map">
+        Map
+      </a>
+
+      <a className="menu-item" href="/truck">
+        Truck Home
+      </a>
+    </Menu>
+  );
+};
