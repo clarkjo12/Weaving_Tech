adaeef9cd2e8c5059a41639f89b78e8f2ac4c462
clarkjo12 <clarkjo12@gmail.com>
2019-02-09 13:54:17 -0500

    Update config.js

--- passport/config.js
+++ passport/config.js
@@ -2,12 +2,12 @@ module.exports = {
     "facebookAuth": {
         "clientID": "1173816946114463",
         "clientSecret": "e98226bd546e9e09a1f93b1e78cd4dcd",
-        "callbackURL": "http: //localhost:3000/auth/facebook/callback",
-        "profileURL": "https: //graph.facebook.com/v2.5/me?fields=first_name,last_name,email"
+        "callbackURL": "/auth/facebook/callback",
+        "profileURL": "https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email"
     },
     "googleAuth": {
         "clientID": "645637599865-klo62iomgapcod16tke26mo0fub29n3k.apps.googleusercontent.com",
         "clientSecret": "Vr7ogFpK1KvCIuJtFgBZgv-Y",
-        "callbackURL": "http://localhost:3000/auth/google/callback"
+        "callbackURL": "/auth/google/callback"
     }
 }
